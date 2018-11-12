d049f6f840394193e44ad929767262768abc922e
andreifurculesteanu <andrei.furcu@gmail.com>
2018-11-12 11:09:20 +0100

    Commit doGet() - Mejorado

--- Ahorcado/WebContent/juego.jsp
+++ Ahorcado/WebContent/juego.jsp
@@ -1,5 +1,9 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
-
+<%
+	String palabra = (String) request.getAttribute("palabra");
+	int fallosDisponibles = (Integer) request.getAttribute("fallosDisponibles");
+	String[] letrasConGuiones = (String []) request.getAttribute("letrasConGuiones"); 
+%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 	<head>
@@ -9,7 +13,15 @@
 	<body>
 		<h1 align="center"> Juego ahorcado </h1>
 	 	<form method="post" action="JuegoAhorcado">
+	 		Palabra a adivinar: <% for (int i = 0; i < letrasConGuiones.length; i++) { %>
+	 							 	<%= letrasConGuiones[i]%>
+	 							<% } %>
+	 			<br/>
 	 		
+	 		Introduce una letra: <input type="text" name="letraIntroducida">
+	 		<br/>
+			<br/>
+			<input type="submit" value="Probar letra">
 	 	</form>
 	</body>
 </html>
--- Ahorcado/src/JuegoAhorcado.java
+++ Ahorcado/src/JuegoAhorcado.java
@@ -1,5 +1,3 @@
-
-
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
@@ -24,36 +22,74 @@ public class JuegoAhorcado extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-
+    
+    //genero la palabra
+    String palabra = "";
+    
+    
+    
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// se crea una sesión asociada a la petición
-        HttpSession laSesion = request.getSession(true);
-        
-        //genero la palabra
-        String palabra = Utilidades.generaPalabra();
-        int intentos = 6;
-        String letras[] = palabra.split("");
-        //se pasan las variables al jsp
-        request.setAttribute("palabra", palabra);       
-        request.setAttribute("intentos", intentos);       
-        request.setAttribute("letras", letras);       
-
-        
-        /* Las 3 siguientes lineas delegan al JSP pintar el formulario (segun el doGet)*/
-		String vista = "/juego.jsp";
-    	RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(vista);
-        dispatcher.forward(request, response); 
-        
+        try {
+			
+        	
+        	HttpSession laSesion = request.getSession(true);
+	        
+	        //genero la palabra
+        	palabra = Utilidades.generaPalabra();
+	        int fallosDisponibles = 6;
+	        String letrasConGuiones[] = new String[palabra.length()]; //array de letras pero con guiones
+	        
+	        
+	        for (int i = 0; i < letrasConGuiones.length; i++) {
+	        	letrasConGuiones[i] = "_";
+	        }
+	        
+	        //se pasan las variables al jsp
+	        request.setAttribute("palabra", palabra);       
+	        request.setAttribute("fallosDisponibles", fallosDisponibles);         
+	        request.setAttribute("letrasConGuiones", letrasConGuiones); 
+	        
+	        //meto en la sesion: palabra, fallos dispopnibles y array de letras de la palabra
+	        laSesion.setAttribute("palabra", palabra);
+	        laSesion.setAttribute("fallosDisponibles", fallosDisponibles);
+	        laSesion.setAttribute("letrasConGuiones", letrasConGuiones);
+	        
+	        /* Las 3 siguientes lineas delegan al JSP pintar el formulario (segun el doGet)*/
+			String vista = "/juego.jsp";
+	    	RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(vista);
+	        dispatcher.forward(request, response); 
+	        
+        } catch (Exception e) {
+        	e.getMessage();
+        }
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		try {
+			//Lo primero es recuperar la sesion 
+			HttpSession laSesion = request.getSession(true);
+			//Se recuperan las variables guardadas en la sesion
+			String palabra = (String) laSesion.getAttribute("palabra");
+			int intentos = (int) laSesion.getAttribute("intentos");
+			String[] letras = (String[]) laSesion.getAttribute("letras");
+			
+			//recupero letra introducida en el "formulario"
+			String letraIntroducida = request.getParameter("letraIntroducida");
+			
+			
+			
+			
+			
+		} catch (Exception e) {
+			e.getMessage();
+		}
 	}
 
 }
