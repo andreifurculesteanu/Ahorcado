71ac6a899c919f22740bb8a745e313a57c1f0618
andreifurculesteanu <andrei.furcu@gmail.com>
2018-11-12 11:40:50 +0100

    Reubicación de archivos tras pull

--- Ahorcado/src/paqueteUtilidades/Utilidades.java
+++ Ahorcado/src/paqueteUtilidades/Utilidades.java
@@ -3,7 +3,7 @@ public class Utilidades {
 	
 	public static String generaPalabra() {
 		String[] arrayPalabras = {"andrei", "daniel", "adrian", "miguel", "maria", "david"};
-		int aleatorio = (int) Math.random()*arrayPalabras.length;
+		int aleatorio = (int) (Math.random()*arrayPalabras.length);
 		String palabra = arrayPalabras[aleatorio];
 		
 		return palabra;
